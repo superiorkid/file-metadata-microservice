b07b78a42a0998cc2e1158dd257c6f5704d49a9e
superiorkid <bayipetani@protonmail.com>
2022-12-12 16:35:29 +0800

    multer file upload

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 var express = require("express");
 var cors = require("cors");
+var multer = require("multer");
 require("dotenv").config();
 
 var app = express();
 
+// set storage
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.filename + "-" + Date.now());
+  },
+});
+
+var upload = multer({ storage });
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", (req, res) => {
-  
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  });
 });
 
 const port = process.env.PORT || 3000;
